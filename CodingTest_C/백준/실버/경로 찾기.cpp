33a912f48c9d735679c22174cd4a3c15dce96684
Yeom MG <threebr123@naver.com>
2024-12-07 16:23:28 +0900

    24/12/07
    C++ Study

--- CodingTest_C/백준/실버/경로 찾기.cpp
+++ CodingTest_C/백준/실버/경로 찾기.cpp
@@ -4,18 +4,17 @@
 using namespace std;
 
 void floyd_warshhall(int board[100][100], int N) {
-	int result[100][100];
 	for (int _v = 0; _v < N; _v++) {
 		for (int v1 = 0; v1 < N; v1++) {
 			for (int v2 = 0; v2 < N; v2++) {
-				result[v1][v2] = board[v1][_v] & board[_v][v2];
+				if (board[v1][_v] && board[_v][v2]) board[v1][v2] = 1;
 			}
 		}
 	}
 
 	for (int row = 0; row < N; row++) {
 		for (int col = 0; col < N; col++) {
-			cout << result[row][col] << " ";
+			cout << board[row][col] << " ";
 		}
 		cout << endl;
 	}
