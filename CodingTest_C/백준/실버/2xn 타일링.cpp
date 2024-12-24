28f76796e51e34dde1b6f223831456575e257921
Yeom MG <threebr123@naver.com>
2024-12-24 13:52:45 +0900

    24/12/24
    C++ Study

---
+++ CodingTest_C/백준/실버/2xn 타일링.cpp
@@ -0,0 +1,19 @@
+// 11726
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int n;
+	cin >> n;
+	
+	int dp[1001] = { 0, };
+
+	dp[1] = 1; dp[2] = 2;
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+	}
+
+	cout << dp[n];
+}
