5d1398f3a495ec38c945d32796c81414fd4b954d
Yeom MG <threebr123@naver.com>
2025-09-04 18:46:23 +0900

    25/09/04
    C++ Study

--- CodingTest_C/백준/골드/케이크 자르기.cpp
+++ CodingTest_C/백준/골드/케이크 자르기.cpp
@@ -11,25 +11,28 @@ vector<int> cut_point;
 vector<int> cnt;
 
 int solution(int goal_cnt) {
-	int left = 0;
+	int left = 1;
 	int right = L;
+	int ans = right;
 	while (left <= right) {
 		int mid = (left + right) / 2;
 		int prev = 0; int cut_count = 0;
 		for (int i = 1; i <= M + 1; i++) {
-			if (cut_point[i] - prev >= mid) {
+			// 마지막으로 커팅하고 생기는 조각이 mid보다 작은 경우까지 고려
+			if (cut_point[i] - prev >= mid && L - cut_point[i] >= mid) {
 				cut_count++;
 				prev = cut_point[i];
 			}
 		}
 
-		if (cut_count > goal_cnt) 
+		if (cut_count >= goal_cnt) {
+			ans = mid;
 			left = mid + 1;
+		}
 		else 
 			right = mid - 1;
-		
 	}
-	return right;
+	return ans;
 }
 
 int main(void) {
@@ -47,6 +50,4 @@ int main(void) {
 		cin >> cnt[i];
 		cout << solution(cnt[i]) << endl;;
 	}
-	
-
 }
